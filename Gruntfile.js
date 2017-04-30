d57310b9b5384dcd07b343f6eb9c5e07ceab776d
sharkykh <sharkykh@gmail.com>
2017-05-01 02:07:24 +0300

    Add grunt tasks for network timezones list

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,17 +9,48 @@ module.exports = function(grunt) {
       files: ['Gruntfile.js']
     },
     watch: {
-      files: ['<%= jsonlint.files %>', '<%=jshint.files %>'],
-      tasks: ['jshint', 'jsonlint']
+      files: ['<%= jsonlint.files %>', '<%=jshint.files %>', 'sb_network_timezones/network_timezones.txt'],
+      tasks: ['jshint', 'jsonlint', 'tzlint']
+    },
+    exec: {
+        updateNetworks: {cmd: 'python sb_network_timezones/update_networks_list.py'}
     }
   });
 
   grunt.loadNpmTasks('grunt-jsonlint');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-exec');
 
-  grunt.registerTask('test', ['jshint', 'jsonlint']);
+  grunt.registerTask('test', ['jshint', 'jsonlint', 'tzlint']);
 
-  grunt.registerTask('default', ['jshint', 'jsonlint']);
+  grunt.registerTask('default', ['jshint', 'jsonlint', 'tzlint']);
+  
+  grunt.registerTask('tzlint', function() {
+    missing = 0;
+    buf = grunt.file.read('sb_network_timezones/network_timezones.txt');
+    if (! /$\n/gm.test(buf)) {
+      grunt.fatal('Linefeed has to be \\n');
+    }
+    lines = buf.trim().split('\n');
+    lines.forEach(function(line) {
+      if (! /^.+:[\w/]*$/.test(line)) {
+        grunt.fatal('Syntax error: [ ' + line + ' ]');
+      }
 
+      sep = line.lastIndexOf(':');
+      network = line.slice(0, sep);
+      timezone = line.slice(sep + 1);
+
+      if (!timezone) {
+        missing += 1;
+      }
+    });
+    
+    grunt.log.ok('Network timezones file successfully verified.');
+    grunt.log.ok('Total networks: ' + lines.length);
+    grunt.log.warn('Missing time zones: ' + missing);
+  });
+  
+  grunt.registerTask('update-networks', ['exec:updateNetworks']);
 };
